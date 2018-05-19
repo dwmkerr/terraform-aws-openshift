e6018a301395008d1cb6181d16f824885597659d
nalbam <me@nalbam.com>
2018-05-19 14:01:17 +0900

    subnetaz

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ module "openshift" {
   region          = "${var.region}"
   amisize         = "t2.large"    //  Smallest that meets the min specs for OS
   vpc_cidr        = "10.0.0.0/16"
-  subnetaz        = "${var.subnetaz}"
   subnet_cidr     = "10.0.1.0/24"
   key_name        = "openshift"
   public_key_path = "${var.public_key_path}"
